09fb540590cce0ab61b81c2ffffd74aa1a10ebd3
Rob Taglang <minitags@comcast.net>
2016-10-10 23:26:13 -0400

    Deleted webpack config in favor of gulp task

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry : "./js/main.js",
-    output : {
-        path : __dirname,
-        filename : 'js/build/bundle.js',
-        libraryTarget : 'var',
-        library : 'Blog'
-    }
-};
